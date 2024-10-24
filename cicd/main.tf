cb14e37e9468e0389199857ae5d47e704b2516c3
chavavenkat <chavavenkatarajesh@gmail.com>
2024-10-24 18:18:15 +0530

    first

--- cicd/main.tf
+++ cicd/main.tf
@@ -14,6 +14,23 @@ module "jenkins" {
 }
 
 
+module "jenkins-agent" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "jenkins-agent"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0c76aed609a9d7308"]
+  subnet_id              = "subnet-0d0d98dc2ce27610f"
+  ami                    = data.aws_ami.ami_info.id
+  user_data              = file("jenkins-agent.sh")
+  tags = {
+    Name = "jenkins-agent"
+  }
+
+}
+
+
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 2.0"
@@ -29,6 +46,15 @@ module "records" {
         module.jenkins.public_ip
       ]
       allow_overwrite = true
-    }
+    },
+    {
+      name = "jenkins-agent"
+      type = "A"
+      ttl  = 1
+      records = [
+        module.jenkins_agent.private_ip
+      ]
+      allow_overwrite = true
+    },
   ]
 }
